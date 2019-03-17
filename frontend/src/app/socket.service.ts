5144c87ca89db42382b7841d778fe38176d5a92f
System Administrator <root@Dhanasekarans-MacBook-Air.local>
2019-03-17 19:01:33 +0530

    service and count

---
+++ frontend/src/app/socket.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Observable,of } from 'rxjs/Observable';
+import * as io from 'socket.io-client';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+
+	private socket = io("http://localhost:3000")	
+  constructor() { }
+
+  getCount = () => {
+  	const count = new Observable((observer) => {
+  		this.socket.on('count',(data) => {
+  			observer.next(data)
+  		});
+  	})
+
+  	return count;
+  }
+
+}
