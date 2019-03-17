b01d612fac659b1f1949d976fc8389c119d2185b
System Administrator <root@Dhanasekarans-MacBook-Air.local>
2019-03-17 18:43:05 +0530

    init commit

---
+++ server/app.js
@@ -0,0 +1,34 @@
+let app = require('express')();
+let bodyparser = require('body-parser');
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+
+app.use(bodyparser.json())
+
+app.get('/',(req,res) => {
+	console.log('hai')
+	io.on('connection', (socket) => {
+	  console.log('socket connected');
+	  
+	  socket.on('disconnect', function(){
+	    console.log('socket disconnected');
+	  });
+	  
+	  io.emit('count', {count: 1});   
+	});
+
+})
+
+app.post('/api/count',(req,res) => {
+	const body = req.body
+	const count = body.count
+	if(count){
+		io.emit('count', {count: count}); 
+		res.send({"count":count})
+	}
+})
+
+
+http.listen(3000, () => {
+  console.log('started on port 3000');
+});
