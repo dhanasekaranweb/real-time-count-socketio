5451699f9a90932da67ba65a768affa96afcd220
System Administrator <root@Dhanasekarans-MacBook-Air.local>
2019-03-17 19:04:02 +0530

    count return

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { SocketService } from './socket.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+  private count;
+
+  constructor(private socketservice:SocketService){}
+
+  ngOnInit(){
+  	this.socketservice.getCount().subscribe(message => {
+      this.count = message.count
+      console.log(message)
+    })
+  	
+  }
 }
